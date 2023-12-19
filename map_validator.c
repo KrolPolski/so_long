85d1659c09cf8b1e009e183d5ba1072219e6ed48
Ryan Boudwin <rboudwin@student.hive.fi>
2023-12-19 11:02:39 +0200

    added test to check for rectangular maps

--- map_validator.c
+++ map_validator.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 09:35:48 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/12/19 10:14:26 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/12/19 11:01:21 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,24 @@ static int	initial_checks(t_map *map)
 	}
 	return (1);
 }
-/*static int	check_shape(t_map *map)
+static int	check_shape(t_map *map)
 {
+	int		curr_line_length;
+	t_list	*curr;
+
+	curr = map->line_list;
+	while (curr->next != NULL)
+	{
+		curr_line_length = ft_strlen(curr->content);
+		if (curr_line_length != map->line_length)
+			return (0);
+		curr = curr->next;
+	}
+	curr_line_length = ft_strlen(curr->content);
+	if (curr_line_length != map->line_length)
+		return (0);
 	return (1);
-}*/
+}
 /*static int	populate_grid(t_map *map)
 {
 	//how much memory to allocate? substrings are taken care of, but how many strings need to be in the array?
@@ -56,7 +70,7 @@ int	read_map(t_map *map)
 	if (current_line)
 	{
 		(map->line_list) = ft_lstnew(current_line);
-		ft_printf("First line is '%s'\n", map->line_list->content);
+		//ft_printf("First line is '%s'\n", map->line_list->content);
 		while (current_line)
 		{
 			current_line = get_next_line(map->fd);
@@ -64,13 +78,14 @@ int	read_map(t_map *map)
 			{
 				current_lst = ft_lstnew(current_line);
 				ft_lstadd_back(&map->line_list, current_lst);
-				ft_printf("Next line is '%s'\n", current_lst->content);
+		//		ft_printf("Next line is '%s'\n", current_lst->content);
 			}
 		}
 	}
-	map->i = ft_lstsize(map->line_list);
-ft_printf("there are '%d' nodes in the list", map->i);
-// add error check
+	map->line_count = ft_lstsize(map->line_list);
+	map->line_length = ft_strlen(map->line_list->content);
+//ft_printf("there are '%d' nodes in the list", map->line_count);
+// add error check for malloc failures and whatnot
 	return (1);
 }
 int	map_validator(t_map *map)
@@ -79,8 +94,11 @@ int	map_validator(t_map *map)
 		return (0);
 	if (!read_map(map))
 		return (0);
-	//if (!check_shape(map))
-	//	return (0);
+	if (!check_shape(map))
+	{
+		ft_printf("The map is not rectangular\n");
+		return (0);
+	}
 	// check for rectangular shape
 	// Check for exactly 1 exit
 	// Check for at least one collectible
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 10:50:45 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/12/19 09:59:42 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/12/19 10:56:30 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ typedef struct s_map
 	char		**grid;
 	int			i;
 	t_list		*line_list;
+	int			line_count;
+	int			line_length;
 }	t_map;
 int	map_validator(t_map *map);
 #endif
