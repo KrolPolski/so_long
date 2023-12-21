3f605b31e7618e103de00a17665db8462efa275f
Ryan Boudwin <rboudwin@student.hive.fi>
2023-12-21 13:53:34 +0200

    character can now move freely with WASD

--- movement.c
+++ movement.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 12:17:00 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/12/21 13:46:37 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/12/21 13:52:18 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,30 @@ void	move_player(t_map *map, t_img *img, char c)
 {
 	if (img)
 		if (map)
-		ft_printf("We managed to ignore CCC\n");
+		ft_printf("We managed to ignore CCC unused paramater warnings\n");
 	ft_printf("We are running move_player with '%c' as a parameter\n", c);
 	if (c == 'L')
 	{
 		ft_printf("We detected intention to move left");//from x:%d and width of %d\n", map->charx, img->img->width);
+		img->img_m->instances[0].x -= map->tile_sq;
+		map->charx--;
 	}
 	if (c == 'R')
 	{
 		ft_printf("We detected intention to move right\n");
+		img->img_m->instances[0].x += map->tile_sq;
+		map->charx++;
 	}
 	if (c == 'U')
 	{
 		ft_printf("We detected intention to move up\n");
+		img->img_m->instances[0].y -= map->tile_sq;
+		map->chary--;
 	}
 	if (c == 'D')
 	{
 		ft_printf("We detected intention to move down\n");
+		img->img_m->instances[0].y += map->tile_sq;
+		map->chary++;
 	}
 }
