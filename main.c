55172971e0ef26f9609cdcfe35b41c48089a877a
Ryan Boudwin <rboudwin@student.hive.fi>
2023-12-22 10:44:04 +0200

    Split arg validation logic into its own function in main.c

--- main.c
+++ main.c
@@ -6,31 +6,35 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 10:50:19 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/12/22 09:42:57 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/12/22 10:43:21 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 //TODO: libft doesn't always make the right way, left out bonuses. check on this
-//TODO: We have to trim \n from the array grid or everything breaks
-int	main(int argc, const char **argv)
+void	check_args(int argc, const char **argv, t_map *map)
 {
-	mlx_t		*mlx;
-	t_map		map;
-	t_img		p;
-
-	map.img = &p;
 	if (argc <= 1)
 	{
 		ft_printf("Please specify a *.ber file to read: so_long <filename>\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	if (argc > 2)
 	{
 		ft_printf("Too many parameters\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-	map.filename = argv[1];
+	map->filename = argv[1];
+}
+
+int	main(int argc, const char **argv)
+{
+	mlx_t	*mlx;
+	t_map	map;
+	t_img	p;
+
+	map.img = &p;
+	check_args(argc, argv, &map);
 	if (!map_validator(&map))
 		return (EXIT_FAILURE);
 	mlx = mlx_init(1366, 768, "so_long", true);
