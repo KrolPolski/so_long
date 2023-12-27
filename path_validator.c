8de43c5d851ae3e0bc5b28d353b2679aeef3b511
Ryan Boudwin <rboudwin@student.hive.fi>
2023-12-27 18:31:08 +0200

    removed debug print lines

--- path_validator.c
+++ path_validator.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 10:26:34 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/12/27 18:01:19 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/12/27 18:30:20 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,10 @@ void	check_square(t_map *map, int y, int x)
 	if (map->grid2[y][x - 1] == 'C')
 		resolve_collectible(map, y, x - 1);
 	map->grid2[y][x] = '*';
-	ft_printf("We checked a square\n");
-	int i;
-	i = 0;
-	while (map->grid2[i] != NULL)
-		ft_printf("%s\n", map->grid2[i++]);
 }
 
 int	collect_exit(t_map *map)
 {
-	ft_printf("Collect_exit was called and map->curr_col is %d\n", map->curr_col);
 	if (map->curr_col > 0)
 		return (0);
 	if (map->grid2[map->exity][map->exitx] == 'E')
@@ -76,7 +70,6 @@ int	mark_paths(t_map *map)
 {
 	int	i;
 
-	ft_printf("About to start marking paths\n");
 	i = 0;
 	map->curr_col = map->collectibles;
 	check_square(map, map->y, map->x);
@@ -95,7 +88,6 @@ int	mark_paths(t_map *map)
 
 int	check_valid_path(t_map *map)
 {
-	ft_printf("Checking for valid path\n");
 	map->grid2 = malloc(sizeof(char *) * map->line_count + 1);
 	map->y = 0;
 	while (map->grid[map->y] != NULL)
@@ -114,6 +106,5 @@ int	check_valid_path(t_map *map)
 	}
 	free_grid(map, 2);
 	map->y = 0;
-	ft_printf("About to conclude the path is valid\n");
 	return (1);
 }
